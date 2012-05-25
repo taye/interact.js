f49f7e5cad4c83abc3a85e383a0849be0a71e40c
Taye Adeyemi <adeyemit@tcd.ie>
2012-05-25 16:39:16 +0000

    further documentation

--- interact.js
+++ interact.js
@@ -3,18 +3,19 @@
 /** @namespace */
 window.interact = (function () {
 	"use strict";
-	/** @private */
+
 	var prevX = 0,
 		prevY = 0,
 		interactNodes = [],
 		nodeStyle,
-		target = 0,
+		target = null,
 		supportsTouch = 'createTouch' in document,
 		mouseIsDown = false,
 		downEvent,
 		upEvent,
 		moveEvent,
 		edgeWidth = supportsTouch? 30 : 10,
+		typeErr = new TypeError('Type Error'),
 		events = {
 			add: function (target, type, listener, useCapture) {
 				if (target.events === undefined) {
@@ -47,6 +48,7 @@ window.interact = (function () {
 				}
 			}
 		};
+
 	/** @private */
 	function xResize(event) {
 		event.preventDefault();
@@ -61,7 +63,7 @@ window.interact = (function () {
 			prevX = x;
 		}
 	}
-	
+
 	/** @private */
 	function yResize(event) {
 		event.preventDefault();
@@ -75,7 +77,7 @@ window.interact = (function () {
 			prevY = y;
 		}
 	}
-	
+
 	/** @private */
 	function xyResize(event) {
 		event.preventDefault();
@@ -91,7 +93,7 @@ window.interact = (function () {
 			prevY = y;
 		}
 	}
-	// Should change this so devices with mouse and touch can use both
+	/* Should change this so devices with mouse and touch can use both */
 	if (supportsTouch) {
 		downEvent = 'touchstart',
 		upEvent = 'touchend',
@@ -102,7 +104,7 @@ window.interact = (function () {
 		upEvent = 'mouseup',
 		moveEvent = 'mousemove';
 	}
-	
+
 	/** @private */
 	function docMouseUp (event) {
 		console.log('target = ' + target);
@@ -115,7 +117,7 @@ window.interact = (function () {
 		clearTarget();
 		events.add(document, moveEvent, mouseMove);
 	}
-	
+
 	/** @private */
 	function mouseMove(event) {
 		if (event.target.actions && (event.target.actions.resize || event.target.actions.drag)) {
@@ -133,7 +135,7 @@ window.interact = (function () {
 			}
 		}
 	};
-	
+
 	/** @private */
 	function mouseDown(event) {
 		mouseIsDown = true;
@@ -176,23 +178,23 @@ window.interact = (function () {
 		return false;
 		}
 	}
-	
+
 	/** @private */
 	function give(parent, child) {
 		return parent.appendChild(child);
 	}
-	
+
 	/** @private */
 	function make(tag, parent) {
 		return parent?	parent.appendChild(document.createElement(tag)):
 				parent.createElement(tag);
 	}
-	
+
 	/** @private */
 	function disown(element) {
 		return element.parentElement.removeChild(element);
 	}
-	
+
 	/** @private */
 	function removeClass(element, className) {
 		element.className = 
@@ -207,7 +209,9 @@ window.interact = (function () {
 	}
 
 	/**
-	 * @class Vector functino for location and dimension
+	 * @class Vector Class for locations and dimensions
+	 * @param {number} x Value of X ordinate
+	 * @param {number} y Value of Y ordinate
 	 */
 	function Vector(x, y) {
 		this.x = x || 0;
@@ -236,9 +240,13 @@ window.interact = (function () {
 	};
 
 	/**
-	 * @constructor
+	 * @class Node for demonstrating interact functionality
+	 * @param {number} x Horizontal position
+	 * @param {number} y Vertical position
+	 * @param {number} w Element width
+	 * @param {number} h Element height
 	 */
-	function interactNode(x, y, w, h) {
+	function demoNode(x, y, w, h) {
 		this.location = new Vector(x, y);
 		this.width = w || 0;
 		this.height = h || 0;
@@ -265,18 +273,25 @@ window.interact = (function () {
 		}
 	}
 
-	interactNode.prototype.setSize = function (x, y) {
-		if (!x || !y) {
-			return;
+	demoNode.prototype.setSize = function (x, y) {
+		if (x && x.constructor.name == 'Vector') {
+			this.width =  x.x;
+			this.height =  x.y;
 		}
-		
-		this.width =  x;
-		this.height =  y;
+		else {
+			if (x == null || y == null) {
+				typeErr.name = 'Incorrect parameter types';
+				typeErr.message = 'demoNode.setSize Parameters must be a single Vector or two numbers.';
+				throw (typeErr);
+			}
+			this.width = x;
+			this.height = y;
+		}
 		this.element.style.setProperty('width', Math.max(x, 20) + 'px', ''); 
 		this.element.style.setProperty('height', Math.max(y, 20) + 'px', ''); 
 	};
 
-	interactNode.prototype.position = function (x, y) {
+	demoNode.prototype.position = function (x, y) {
 		if (x === undefined || y === undefined) {
 			return;
 		}
@@ -285,13 +300,15 @@ window.interact = (function () {
 		this.element.style.setProperty('left', x + 'px', '');
 		this.element.style.setProperty('top', y + 'px', '');
 	};
-	
+
+	/** @private */
 	function bringToFront(element) {
 		// Very lazy
 		var parent = element.parentElement;
 		give(parent, disown(element));
 	}
-	
+
+	/** @private */
 	function clearTarget() {
 		if (target) {
 			removeClass(target.element, 'interact-target');
@@ -299,16 +316,31 @@ window.interact = (function () {
 		target = null;
 	}
 
-// Public:
+	/**
+	 * @name interact
+	 * 
+	 */
 	var interact = {};
 	interact.nodes = [];
-	
-	interact.test = function () {
+
+	/**
+	 * @function
+	 * @memberOf interact
+	 * @description Display debugging data in the browser console
+	 */
+	interact.debug = function () {
 		console.log('target   : ' + target);
 		console.log('prevX    : ' + prevX);
 		console.log('prevY    : ' + prevY);
+		throw (typeErr);
 	}
-	
+
+	/**
+	 * @function
+	 * @description Add an element to the list of interact nodes
+	 * @param {object HTMLElement} element The DOM Element that will be added
+	 * @param {object} options An object with boolean properties
+	 */
 	interact.set = function (element, options) {
 		var	newNode = {
 				element: element,
@@ -322,6 +354,12 @@ window.interact = (function () {
 		// different things to be done for normal nodes and graphics nodes
 	};
 	
+
+	/**
+	 * @function
+	 * @description Remove an element from the list of interact nodes
+	 * @param {object HTMLElement} element The DOM Element that will be removed
+	 */
 	interact.unset = function (element) {
 		var i;
 		for (i = 0; i < interactNodes.length; i++) {
@@ -331,6 +369,9 @@ window.interact = (function () {
 		}
 	};
 	
+	/**
+	 * @description Contains drag functions
+	 */
 	interact.drag = {
 		xyDrag: function (event) {
 			event.preventDefault();
@@ -346,6 +387,13 @@ window.interact = (function () {
 		}
 	};
 	
+
+	/**
+	 * @function
+	 * @description introduce random draggable, resizeable nodes to the document (for testing)
+	 * @param {number} [n] The number of nodes to be added (default
+	 * @param {object} options An object with boolean properties
+	 */
 	interact.randomNodes = function (n, parent) {
 		n = n || 10;
 		parent = parent || document.body;
@@ -355,11 +403,11 @@ window.interact = (function () {
 				var par = interact.nodes[i].element.parentNode;
 				
 				par.removeChild(interact.nodes[i].element);
-				interact.nodes[i] = new interactNode(Math.random()*(window.screen.width - 20), Math.random()*(window.screen.height - 20), Math.random()* 200, Math.random()* 200);
+				interact.nodes[i] = new demoNode(Math.random()*(window.screen.width - 20), Math.random()*(window.screen.height - 20), Math.random()* 200, Math.random()* 200);
 				give(par, interact.nodes[interact].element);
 			}
 			else {
-				interact.nodes.push(new interactNode(Math.random()*(window.screen.width - 20), Math.random()*(window.screen.height - 20), Math.random()* 200, Math.random()* 200));
+				interact.nodes.push(new demoNode(Math.random()*(window.screen.width - 20), Math.random()*(window.screen.height - 20), Math.random()* 200, Math.random()* 200));
 				give(document.body, interact.nodes[i].element);
 			}
 		}
