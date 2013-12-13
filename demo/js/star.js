79851fc63c4c7eebe84844004d4a04ee2b5f7fbb
Taye Adeyemi <dev@taye.me>
2013-12-13 21:15:40 +0000

    Add view box to star.svg and consider doc scale

--- demo/js/star.js
+++ demo/js/star.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
     var sns="http://www.w3.org/2000/svg",
         xns="http://www.w3.org/1999/xlink",
         root = document.getElementById('svg-edit-demo'),
-        star = document.getElementById('edit-star');
+        star = document.getElementById('edit-star'),
+        rootMatrix;
 
     for (var i = 0, len = star.points.numberOfItems; i < len; i++) {
         var handle = root.querySelector('defs .point-handle').cloneNode(true),
@@ -18,12 +21,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     interact('.point-handle')
         .draggable({
+            onstart: function (event) {
+                rootMatrix = root.getScreenCTM();
+            },
             onmove: function (event) {
                 var i = event.target.getAttribute('data-index')|0,
                     point = star.points.getItem(i);
 
-                point.x += event.dx;
-                point.y += event.dy;
+                point.x += event.dx / rootMatrix.a;
+                point.y += event.dy / rootMatrix.d;
 
                 event.target.cx.baseVal.value = point.x;
                 event.target.cy.baseVal.value = point.y;
