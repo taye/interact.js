5b08e5561b25e8c9a3e0f76566587e4d6818eb69
Taye Adeyemi <dev@taye.me>
2013-12-13 23:37:19 +0000

    Add snapping to original star points

--- demo/js/star.js
+++ demo/js/star.js
@@ -5,25 +5,44 @@ document.addEventListener('DOMContentLoaded', function () {
         xns="http://www.w3.org/1999/xlink",
         root = document.getElementById('svg-edit-demo'),
         star = document.getElementById('edit-star'),
-        rootMatrix;
+        rootMatrix,
+        originalPoints = [],
+        transformedPoints = [];
 
     for (var i = 0, len = star.points.numberOfItems; i < len; i++) {
         var handle = root.querySelector('defs .point-handle').cloneNode(true),
-            point = star.points.getItem(i);
+            point = star.points.getItem(i),
+            newPoint = root.createSVGPoint();
 
-        handle.cx.baseVal.value = point.x;
-        handle.cy.baseVal.value = point.y;
+        handle.cx.baseVal.value = newPoint.x = point.x;
+        handle.cy.baseVal.value = newPoint.y = point.y;
 
         handle.setAttribute('data-index', i);
+
+        originalPoints.push(newPoint);
 
         root.appendChild(handle);
     }
 
+    function applyTransforms (event) {
+        rootMatrix = root.getScreenCTM();
+
+        transformedPoints = originalPoints.map(function(point) {
+            return point.matrixTransform(rootMatrix);
+        });
+
+        interact('.point-handle').snap({
+            anchors: transformedPoints,
+            range: 20 * Math.max(rootMatrix.a, rootMatrix.d)
+        });
+    }
+
+    interact(root)
+        .on('mousedown', applyTransforms)
+        .on('touchstart', applyTransforms);
+
     interact('.point-handle')
         .draggable({
-            onstart: function (event) {
-                rootMatrix = root.getScreenCTM();
-            },
             onmove: function (event) {
                 var i = event.target.getAttribute('data-index')|0,
                     point = star.points.getItem(i);
@@ -34,5 +53,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 event.target.cx.baseVal.value = point.x;
                 event.target.cy.baseVal.value = point.y;
             }
+        })
+        .snap({
+            mode: 'anchor',
+            anchors: originalPoints,
+            range: 10
         });
 });
