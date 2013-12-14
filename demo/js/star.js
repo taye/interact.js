e9194a96ffd39fba5f44f700665c25d752e2c2f3
Taye Adeyemi <dev@taye.me>
2013-12-14 20:15:43 +0000

    Use SVG <use> elements in star demo

--- demo/js/star.js
+++ demo/js/star.js
@@ -10,12 +10,15 @@ document.addEventListener('DOMContentLoaded', function () {
         transformedPoints = [];
 
     for (var i = 0, len = star.points.numberOfItems; i < len; i++) {
-        var handle = root.querySelector('defs .point-handle').cloneNode(true),
+        var handle = document.createElementNS(sns, 'use'),
             point = star.points.getItem(i),
             newPoint = root.createSVGPoint();
 
-        handle.cx.baseVal.value = newPoint.x = point.x;
-        handle.cy.baseVal.value = newPoint.y = point.y;
+        handle.setAttributeNS(xns, 'href', '#point-handle');
+        handle.setAttribute('class', 'point-handle');
+
+        handle.x.baseVal.value = newPoint.x = point.x;
+        handle.y.baseVal.value = newPoint.y = point.y;
 
         handle.setAttribute('data-index', i);
 
@@ -50,8 +53,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 point.x += event.dx / rootMatrix.a;
                 point.y += event.dy / rootMatrix.d;
 
-                event.target.cx.baseVal.value = point.x;
-                event.target.cy.baseVal.value = point.y;
+                event.target.x.baseVal.value = point.x;
+                event.target.y.baseVal.value = point.y;
             }
         })
         .snap({
