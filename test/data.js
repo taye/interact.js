767ecd30169d8a1a208c0abf812a9b01a738d455
Taye Adeyemi <dev@taye.me>
2014-04-12 18:18:08 +0100

    Modify tests for IE10+ PointerEvents

--- test/data.js
+++ test/data.js
@@ -1,40 +1,70 @@
+if (window.PointerEvent) {
+    var types = {
+        down: 'pointerdown',
+        move: 'pointermove',
+        up: 'pointerup'
+    };
+}
+else {
+    var types = {
+        down: 'touchstart',
+        move: 'touchmove',
+        up: 'touchend'
+    };
+}
+
 window.data = {
     downMove2Up: [
-        { x: 120, y:   55, type: 'mousedown' },
-        { x:   0, y: -200, type: 'mousemove' },
-        { x:  80, y: -100, type: 'mousemove' },
-        { x:  80, y: -100, type: 'mouseup' }
+        { x: 120, y:   55, type: 'mousedown', pointerId: 1 },
+        { x:   0, y: -200, type: 'mousemove', pointerId: 1 },
+        { x:  80, y: -100, type: 'mousemove', pointerId: 1 },
+        { x:  80, y: -100, type: 'mouseup', pointerId: 1 }
     ],
     touch2Move2End2: [
         {
-            type: 'touchstart',
+            type: types.down,
+            pointerId: 1,
+            x: -23,
+            y: -78,
             touches: [
                 { x: -23, y: -78 }
             ]
         },
         {
-            type: 'touchstart',
+            type: types.down,
+            pointerId: 2,
+            x: -100,
+            y: -100,
             touches: [
                 { x: -100, y: -100 },
                 { x:  100, y:  100 }
             ]
         },
         {
-            type: 'touchmove',
+            type: types.move,
+            pointerId: 1,
+            x: -50,
+            y: -50,
             touches: [
                 { x:  50, y: -50 },
                 { x: 100, y: 100 }
             ]
         },
         {
-            type: 'touchmove',
+            type: types.move,
+            pointerId: 2,
+            x: 50,
+            y: -50,
             touches: [
                 { x:  50, y: -50 },
                 { x: -50, y:  50 }
             ]
         },
         {
-            type: 'touchend',
+            type: types.up,
+            pointerId: 1,
+            x: -50,
+            y:  50,
             touches: [
                 { x: -50, y:  50 }
             ],
@@ -43,7 +73,10 @@ window.data = {
             ]
         },
         {
-            type: 'touchend',
+            type: types.up,
+            pointerId: 2,
+            x: -50,
+            y:  50,
             touches: [],
             changed: [
                 { x: -50, y:  50 }
