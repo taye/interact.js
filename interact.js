a828946922dc19909de759c23f2fc2f4f4eb43a2
Taye Adeyemi <dev@taye.me>
2014-09-04 13:58:34 +0100

    Remove indexOf and contains in the events module

--- interact.js
+++ interact.js
@@ -581,20 +581,6 @@
                 }
             }
 
-            function indexOf (array, target) {
-                for (var i = 0, len = array.length; i < len; i++) {
-                    if (array[i] === target) {
-                        return i;
-                    }
-                }
-
-                return -1;
-            }
-
-            function contains (array, target) {
-                return indexOf(array, target) !== -1;
-            }
-
             function preventDef () {
                 this.returnValue = false;
             }
