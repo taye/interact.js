cc148197a0e855ca12045091fb0cf469bb956698
Taye Adeyemi <dev@taye.me>
2014-11-22 02:50:30 +0000

    Fix tap and doubletap with touch input

--- interact.js
+++ interact.js
@@ -2379,7 +2379,7 @@
         },
 
         collectEventTargets: function (pointer, event, eventTarget, eventType) {
-            var pointerIndex = this.mouse? 0 : indexOf(getPointerId(pointer));
+            var pointerIndex = this.mouse? 0 : indexOf(this.pointerIds, getPointerId(pointer));
 
             // do not fire a tap event if the pointer was moved before being lifted
             if (eventType === 'tap' && (this.pointerWasMoved
