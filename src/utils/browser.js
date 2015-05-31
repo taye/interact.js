09a410c2724e75ab12299ce34546611c815e0ff2
stephen-james <stephenjames@me.com>
2015-05-31 21:46:26 +0100

    reformatting browser module

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -1,25 +1,26 @@
 'use strict';
 
-var browser = {},
-    win = require('./window'),
+var win = require('./window'),
     domObjects = require('./domObjects');
 
-// Does the browser support touch input?
-browser.supportsTouch = !!(('ontouchstart' in win) || win.window.DocumentTouch && domObjects.document instanceof win.DocumentTouch);
+var browser = {
+    // Does the browser support touch input?
+    supportsTouch : !!(('ontouchstart' in win) || win.window.DocumentTouch
+        && domObjects.document instanceof win.DocumentTouch),
 
-// Does the browser support PointerEvents
-browser.supportsPointerEvent = !!domObjects.PointerEvent;
+    // Does the browser support PointerEvents
+    supportsPointerEvent : !!domObjects.PointerEvent,
 
-// Opera Mobile must be handled differently
-browser.isOperaMobile = (navigator.appName == 'Opera'
-                                && browser.supportsTouch
-                                && navigator.userAgent.match('Presto'));
+    // Opera Mobile must be handled differently
+    isOperaMobile : (navigator.appName === 'Opera'
+        && browser.supportsTouch
+        && navigator.userAgent.match('Presto')),
 
-// scrolling doesn't change the result of
-// getBoundingClientRect/getClientRects on iOS <=7 but it does on iOS 8
-browser.isIOS7orLower = (/iP(hone|od|ad)/.test(navigator.platform)
-                                && /OS [1-7][^\d]/.test(navigator.appVersion));
+    // scrolling doesn't change the result of
+    // getBoundingClientRect/getClientRects on iOS <=7 but it does on iOS 8
+    isIOS7orLower : (/iP(hone|od|ad)/.test(navigator.platform) && /OS [1-7][^\d]/.test(navigator.appVersion)),
 
-browser.isIe9OrOlder = domObjects.document.all && !win.window.atob;
+    isIe9OrOlder : domObjects.document.all && !win.window.atob
+};
 
 module.exports = browser;
