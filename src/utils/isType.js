1e2ce5a7faf36d193720ef51f1134b4331ab5308
stephen-james <stephenjames@me.com>
2015-06-02 21:27:01 +0100

    readability with object literal for module and single export
    A subjective change, but takes out the repetition of `module.exports.`, may make it easier to grok

--- src/utils/isType.js
+++ src/utils/isType.js
@@ -3,26 +3,37 @@
 var win = require('./window'),
     domObjects = require('./domObjects');
 
-module.exports.isElement = function (o) {
-    if (!o || (typeof o !== 'object')) { return false; }
+var isType = {
+    isElement : function (o) {
+        if (!o || (typeof o !== 'object')) { return false; }
+    
+        var _window = win.getWindow(o) || win.window;
+    
+        return (/object|function/.test(typeof _window.Element)
+            ? o instanceof _window.Element //DOM2
+            : o.nodeType === 1 && typeof o.nodeName === "string");
+    },
+    
+    isWindow   : function (thing) { return !!(thing && thing.Window) && (thing instanceof thing.Window); },
 
-    var _window = win.getWindow(o) || win.window;
+    isDocFrag  : function (thing) { return !!thing && thing instanceof domObjects.DocumentFragment; },
 
-    return (/object|function/.test(typeof _window.Element)
-        ? o instanceof _window.Element //DOM2
-        : o.nodeType === 1 && typeof o.nodeName === "string");
+    isArray    : function (thing) {
+        return isObject(thing)
+            && (typeof thing.length !== undefined)
+            && isFunction(thing.splice);
+    },
+
+    isObject   : function (thing) { return !!thing && (typeof thing === 'object'); },
+
+    isFunction : function (thing) { return typeof thing === 'function'; },
+
+    isNumber   : function (thing) { return typeof thing === 'number'  ; },
+
+    isBool     : function (thing) { return typeof thing === 'boolean' ; },
+
+    isString   : function (thing) { return typeof thing === 'string'  ; }
+    
 };
 
-module.exports.isWindow   = function (thing) { return !!(thing && thing.Window) && (thing instanceof thing.Window); };
-module.exports.isDocFrag  = function (thing) { return !!thing && thing instanceof domObjects.DocumentFragment; };
-module.exports.isArray    = function (thing) {
-    return module.exports.isObject(thing)
-    && (typeof thing.length !== undefined)
-    && module.exports.isFunction(thing.splice);
-};
-module.exports.isObject   = function (thing) { return !!thing && (typeof thing === 'object'); };
-module.exports.isFunction = function (thing) { return typeof thing === 'function'; };
-module.exports.isNumber   = function (thing) { return typeof thing === 'number'  ; };
-module.exports.isBool     = function (thing) { return typeof thing === 'boolean' ; };
-module.exports.isString   = function (thing) { return typeof thing === 'string'  ; };
-
+module.exports = isType;
