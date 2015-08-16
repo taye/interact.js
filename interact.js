736b8a423e8a7a275c8905ed584f05dadd6173a9
Taye Adeyemi <dev@taye.me>
2015-08-16 12:29:29 +0200

    Add `buttons` property to InteractEvents
    Closes #243

--- interact.js
+++ interact.js
@@ -3366,6 +3366,7 @@
         this.shiftKey  = event.shiftKey;
         this.metaKey   = event.metaKey;
         this.button    = event.button;
+        this.buttons   = event.buttons;
         this.target    = element;
         this.t0        = interaction.downTimes[0];
         this.type      = action + (phase || '');
