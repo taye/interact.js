taye/interact.js

commit 5e5a04000cc546916bca45943cc48badc67e2146

Fix incorrect coordinates of actions' first move

interact.js
@@ -1725,13 +1725,17 @@
                 interactions.push(this);
             }
 
+            // set the startCoords if there was no prepared action
+            if (!this.prepared.name) {
+                this.setEventXY(this.startCoords, this.pointers);
+            }
+
             this.prepared.name  = action.name;
             this.prepared.axis  = action.axis;
             this.prepared.edges = action.edges;
             this.target         = interactable;
             this.element        = element;
 
-            this.setEventXY(this.startCoords, this.pointers);
             this.setStartOffsets(action.name, interactable, element);
             this.setModifications(this.startCoords.page);
 

commit 257d5c066dba3a541def977cfc3f162d215d057a

Fix event coords of manually started actions
Broken by 5e5a040

interact.js
@@ -1738,7 +1738,7 @@
 
             // set the startCoords if there was no prepared action
             if (!this.prepared.name) {
-                this.setEventXY(this.startCoords, this.pointers);
+                this.setEventXY(this.startCoords);
             }
 
             this.prepared.name  = action.name;