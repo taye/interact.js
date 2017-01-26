51afcdfe525d9adda057acf4d33655e561e7875c
Taye Adeyemi <dev@taye.me>
2017-01-26 19:04:35 +0100

    test: eslint failAfterError before running tests

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,15 +1,25 @@
 var gulp   = require('gulp');
-var eslint = require('gulp-eslint');
 
 gulp.task('lint', module.exports = function () {
-  return gulp.src(['src/**/*.js'])
+  var eslint = require('gulp-eslint');
+  let sources;
+
+  try {
+    var execSync = require('child_process').execSync;
+    sources = execSync('git ls-files "src/**.js"').toString().split('\n');
+  }
+  catch (e) {
+    sources = ['src/**/*.js'];
+  }
+
+  return gulp.src(sources)
   // eslint() attaches the lint output to the eslint property
   // of the file object so it can be used by other modules.
   .pipe(eslint())
   // eslint.format() outputs the lint results to the console.
   // Alternatively use eslint.formatEach() (see Docs).
-  .pipe(eslint.format())
+  .pipe(eslint.format('table'))
   // To have the process exit with an error code (1) on
-  // lint error, return the stream and pipe to failOnError last.
-  // .pipe(eslint.failOnError());
+  // lint error, return the stream and pipe to failAfterError last.
+  .pipe(eslint.failAfterError());
 });
