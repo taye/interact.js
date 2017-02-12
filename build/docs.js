e418557de4be552af6098785431f03297db30ae4
Taye Adeyemi <dev@taye.me>
2017-02-12 18:44:01 +0000

    build, package.json: improve script args

--- build/docs.js
+++ build/docs.js
@@ -2,16 +2,25 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const replacer = require('./replacer');
 
-mkdirp.sync('dist');
+module.exports = ({ release = process.argv.includes('--release'), stdio = 'inherit' } = {}) => {
+  process.stdout.write('Docs...');
+  mkdirp.sync('dist');
 
-const drjson = replacer(fs.readFileSync('dr.json').toString(), {
-  decorate: false,
-});
+  const drjson = replacer(fs.readFileSync('dr.json').toString(), {
+    release,
+    decorate: false,
+  });
 
-fs.writeFileSync('_dr.json', drjson);
+  fs.writeFileSync('_dr.json', drjson);
 
-require('child_process').spawnSync('npm', ['run', '_dr.js'], {
-  stdio: 'inherit',
-});
+  require('child_process').spawnSync('npm', ['run', '_dr.js'], {
+    stdio,
+  });
 
-fs.unlinkSync('_dr.json');
+  fs.unlinkSync('_dr.json');
+  console.log(' done.');
+};
+
+if (process.argv.includes('--go'))  {
+  module.exports();
+}
--- build/index.js
+++ build/index.js
@@ -17,6 +17,7 @@ const b = browserify(config);
 const pwdRegex = new RegExp(`^${process.env.PWD}.`);
 const release = process.argv.includes('--release');
 const watch = process.argv.includes('--watch');
+const docs = process.argv.includes('--docs')? require('./docs') : null;
 
 if (watch) {
   b.plugin(require('watchify'));
@@ -25,15 +26,31 @@ if (watch) {
   b.on('update', update);
   b.on('log', msg => console.log(msg));
 }
+else {
+  process.on('beforeExit', () => {
+    console.log(' done.');
+  });
+}
 
 function update (ids) {
+  if (docs) {
+    docs({
+      release,
+      stdio: ['ignore', 'ignore', 'inherit'],
+    });
+  }
+
+  if (watch) {
+    console.log('Bundling...');
+  }
+  else {
+    process.stdout.write('Bundling...');
+  }
+
   if (ids) {
     console.log(ids.reduce((formatted, id) => {
       return `${formatted}\n    ${id.replace(pwdRegex, '')}`;
     }, ''));
-  }
-  else {
-    console.log('Bundling...');
   }
 
   bundleProcessor({
