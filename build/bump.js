9c564c49da4c3bde98719dbff4e89167ad4dc51d
Taye Adeyemi <dev@taye.me>
2017-12-01 02:23:15 +0100

    build/bump: allow target version to be supplied

--- build/bump.js
+++ build/bump.js
@@ -5,10 +5,19 @@ const [,, release, prereleaseId] = process.argv;
 let newVersion;
 
 if (release) {
-  newVersion = version.bump({
-    release,
-    prereleaseId,
-  });
+  if (/^(major|minor|patch|premajor|preminor|prepatch|prerelease)$/.test(release)) {
+    newVersion = version.bump({
+      release,
+      prereleaseId,
+    });
+  }
+  else {
+    newVersion = require('semver').clean(release);
+
+    if (newVersion === null) {
+      throw `Invalid version "${release}"`;
+    }
+  }
 }
 // if this was run with no arguments, get the current version with
 // updated build metadata
