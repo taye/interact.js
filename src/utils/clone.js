252cefadc70ee5249be3fc5ee2e8a5c232507da8
Taye Adeyemi <dev@taye.me>
2018-03-12 04:22:49 +0100

    utils/clone: copy array values

--- src/utils/clone.js
+++ src/utils/clone.js
@@ -1,13 +1,22 @@
-import { plainObject } from './is';
+import * as is from './is';
+import * as arr from './arr';
 
 export default function clone (source) {
   const dest = {};
+
   for (const prop in source) {
-    if (plainObject(source[prop])) {
-      dest[prop] = clone(source[prop]);
-    } else {
-      dest[prop] = source[prop];
+    const value = source[prop];
+
+    if (is.plainObject(value)) {
+      dest[prop] = clone(value);
+    }
+    else if (is.array(value)) {
+      dest[prop] = arr.from(value);
+    }
+    else {
+      dest[prop] = value;
     }
   }
+
   return dest;
 }
