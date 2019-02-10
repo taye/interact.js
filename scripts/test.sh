a5a70acffcd434d1c8ce17f3d31988c75629490b
Taye Adeyemi <dev@taye.me>
2019-02-10 18:54:57 +0100

    test: improve setup

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 PKG_DIR=$(dirname $(readlink -f $0))/..
+
 export PATH=$PKG_DIR/node_modules/.bin:$PWD/node_modules/.bin:$PATH
 export NODE_ENV=test
 export TS_NODE_TRANSPILE_ONLY=${TS_NODE_TRANSPILE_ONLY:-0}
@@ -7,8 +8,15 @@ export TS_NODE_PRETTY=${TS_NODE_PRETTY:-1}
 export TS_NODE_COMPILER_OPTIONS=${TS_NODE_COMPILER_OPTIONS:-"{ \"module\": \"commonjs\" }"}
 
 nyc --silent \
-  node ${NODE_ARGS:+"$NODE_ARGS"} \
-  -r ts-node/register \
-  -r $PKG_DIR/packages/types/index.ts \
+  node \
+  ${NODE_ARG:+$NODE_ARG} \
+  --require ts-node/register \
+  --require $PKG_DIR/packages/types/index.ts \
   $PKG_DIR/test/all.ts $@ |
-  tap-spec && nyc report && nyc check-coverage
+  tap-spec;
+
+test_code=$?
+
+nyc report; nyc check-coverage
+
+exit $test_code
--- test/all.ts
+++ test/all.ts
@@ -1,4 +1,6 @@
 // tslint:disable no-var-requires
+require('../packages/types/index')
+
 const glob = require('glob')
 const path = require('path')
 
@@ -19,7 +21,7 @@ function getMatches (pattern) {
   })
 }
 
-(fileArgs.length ? Promise.resolve(fileArgs) : getMatches('**/*.spec.ts')).then((tests) => {
+(fileArgs.length ? Promise.resolve(fileArgs.filter((f) => f === '--')) : getMatches('**/*.spec.ts')).then((tests) => {
   for (const file of tests) {
     require(path.resolve(file))
   }
