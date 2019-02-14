88435f0cd6b2e6139f8b173aa714789cf8215cc2
Taye Adeyemi <dev@taye.me>
2019-02-15 00:39:12 +0100

    feat(autoStart): set interaction.rect on prepared

--- packages/core/Interaction.ts
+++ packages/core/Interaction.ts
@@ -22,8 +22,10 @@ export class Interaction<T extends ActionName = any> {
 
   // the target element of the interactable
   element: Element = null
-  rect: Interact.Rect & Interact.Rect2
-  edges: Partial<Interact.Rect>
+  rect: Interact.Rect & Interact.Rect3
+  edges: {
+    [P in keyof Interact.Rect]?: boolean
+  }
 
   _signals: utils.Signals
 
@@ -152,8 +154,8 @@ export class Interaction<T extends ActionName = any> {
 
     this.interactable = target
     this.element      = element
-    this.rect         = target.getRect(element);
-    this.edges        = this.prepared.edges;
+    this.rect         = target.getRect(element)
+    this.edges        = this.prepared.edges
     this._interacting = this._doPhase({
       interaction: this,
       event: this.downEvent,
@@ -451,16 +453,16 @@ export class Interaction<T extends ActionName = any> {
     }
 
     const iEvent = signalArg.iEvent = this._createPreparedEvent(event, phase, preEnd, type)
-    const { rect } = this;
+    const { rect } = this
 
     if (rect) {
       // update the rect modifications
-      const edges = this.edges || this.prepared.edges || { left: true, right: true, top: true, bottom: true };
+      const edges = this.edges || this.prepared.edges || { left: true, right: true, top: true, bottom: true }
 
-      if (edges.top   ) { rect.top    += iEvent.delta.y; }
-      if (edges.bottom) { rect.bottom += iEvent.delta.y; }
-      if (edges.left  ) { rect.left   += iEvent.delta.x; }
-      if (edges.right ) { rect.right  += iEvent.delta.x; }
+      if (edges.top)    { rect.top    += iEvent.delta.y }
+      if (edges.bottom) { rect.bottom += iEvent.delta.y }
+      if (edges.left)   { rect.left   += iEvent.delta.x }
+      if (edges.right)  { rect.right  += iEvent.delta.x }
     }
 
     this._signals.fire(`action-${phase}`, signalArg)
