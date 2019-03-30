b8afdd0397034874fcc9ffa0dcf6d78517cc7b5e
Taye Adeyemi <dev@taye.me>
2019-03-30 19:08:28 +0100

    chore: improve test scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,16 +7,26 @@ export TS_NODE_TRANSPILE_ONLY=${TS_NODE_TRANSPILE_ONLY:-0}
 export TS_NODE_PRETTY=${TS_NODE_PRETTY:-1}
 export TS_NODE_COMPILER_OPTIONS=${TS_NODE_COMPILER_OPTIONS:-"{ \"module\": \"commonjs\" }"}
 
-nyc --silent \
-  node \
-  ${NODE_ARG:+$NODE_ARG} \
+report=0
+
+! [[ -n $TEST_RUNNER ]] && {
+  report=1
+  TEST_RUNNER=nyc
+  TEST_RUNNER_ARGS=${TEST_RUNNER_ARGS:=--silent}
+  EXEC_COMMAND=${EXEC_COMMAND:=node}
+}
+
+NODE_ENV=test $TEST_RUNNER $TEST_RUNNER_ARGS \
+  $EXEC_COMMAND \
   --require ts-node/register \
   --require $PKG_DIR/packages/types/index.ts \
   $PKG_DIR/test/all.ts $@ |
-  tap-spec;
+  tap-spec
 
 test_code=$?
 
-nyc report; nyc check-coverage
+if [ $report == 1 ]; then
+  nyc report; nyc check-coverage
+fi
 
 exit $test_code
