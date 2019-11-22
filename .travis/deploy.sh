473df52001327ed6e5e904edbabf652b988ebd70
Taye Adeyemi <dev@taye.me>
2019-11-22 15:51:04 +0100

    chore(travis): use $TRAVIS_BUILD_DIR in deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-ROOT=$(dirname $(readlink -f $0))/..
-
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
+chmod 600 $TRAVIS_BUILD_DIR/.travis/id_rsa # Allow read access to the private key
+ssh-add $TRAVIS_BUILD_DIR/.travis/id_rsa # Add the private key to SSH
 
 git config --global push.default matching
 git remote add origin ssh://git@github.com/taye/interact.js.git
