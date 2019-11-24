f78d47db65f995fe132177d49029d2ae126cc3ac
Travis CI User <travis@example.org>
2019-11-24 14:27:07 +0000

    v1.8.0-alpha.4

---
+++ @interactjs/auto-start/index.d.ts
@@ -0,0 +1,6 @@
+import autoStart from './base';
+import dragAxis from './dragAxis';
+import hold from './hold';
+declare function install(scope: any): void;
+declare const id = "auto-start";
+export { id, install, autoStart, hold, dragAxis, };
---
+++ @interactjs/types/index.d.ts
@@ -0,0 +1,1 @@
+/// <reference types="@interactjs/types/types" />
