fd314440b760fe06d092c855fe93628b3c9d29ae
Taye Adeyemi <dev@taye.me>
2019-11-25 22:20:31 +0100

    chore(travis/deploy): fetch tags before release

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,7 @@ chmod 600 $HOME/deploy_key # Allow read access to the private key
 ssh-add $HOME/deploy_key # Add the private key to SSH
 
 git remote set-url origin ssh://git@github.com/taye/interact.js.git
+git fetch --tags
 
 # undo permission changes to bin files
 git checkout -- $TRAVIS_BUILD_DIR/{jsdoc,scripts}
