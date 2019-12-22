49fa6e193aca83ec86710427ad037247e63a22db
Taye Adeyemi <dev@taye.me>
2019-12-22 04:31:11 +0100

    fix(interactjs): remove "?" from interact.{modifiers,snappers,createSnapGrid} definitions

--- @interactjs/interactjs/index.ts
+++ @interactjs/interactjs/index.ts
@@ -6,9 +6,9 @@ import * as snappers from '@interactjs/utils/snappers/index'
 
 declare module '@interactjs/interact/interact' {
   interface InteractStatic {
-    modifiers?: typeof modifiers
-    snappers?: typeof snappers
-    createSnapGrid?: typeof snappers.grid
+    modifiers: typeof modifiers
+    snappers: typeof snappers
+    createSnapGrid: typeof snappers.grid
   }
 }
 
