b0371f60aa35188572a74a9828646a4a599fd9b9
Taye Adeyemi <dev@taye.me>
2020-01-09 17:40:31 +0100

    fix(scripts): fix sources globs

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,10 +1,10 @@
 const { promisify } = require('util')
 const glob = promisify(require('glob'))
 
-const sourcesGlob = '{,@}interactjs/**/*{.ts,.tsx}'
+const sourcesGlob = '{,@}interactjs/**/**/*{.ts,.tsx}'
 const lintIgnoreGlobs = ['**/node_modules/**', '**/*_*', '**/*.d.ts', '**/dist/**']
 const sourcesIgnoreGlobs = [...lintIgnoreGlobs, '**/*.spec.ts']
-const builtFilesGlob = '{**/dist/**,{,@}interactjs/**/*.js{,.map}}'
+const builtFilesGlob = '{**/dist/**,{,@}interactjs/**/**/*.js{,.map}}'
 const builtFilesIgnoreGlobs = ['**/node_modules/**']
 
 const getSources = ({ cwd = process.cwd() } = {}) => glob(
