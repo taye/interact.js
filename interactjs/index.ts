7040bc83ca501392ff05f000354b44aabeffffb0
Taye Adeyemi <dev@taye.me>
2020-05-05 03:24:57 +0200

    fix(interactjs): restore commonjs require without .default

--- interactjs/index.ts
+++ interactjs/index.ts
@@ -2,3 +2,10 @@ import '@interactjs/types'
 import interact from '@interactjs/interactjs/index'
 
 export default interact
+
+if (typeof module === 'object' && !!module) {
+  try { module.exports = interact }
+  catch {}
+}
+
+(interact as any).default = interact
