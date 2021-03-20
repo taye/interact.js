1332a2643d987f39025eb6e734ab6ffd4ffcd413
Taye Adeyemi <dev@taye.me>
2021-03-20 19:04:55 +0100

    chore: remove redundant module.exports assignment

--- packages/interactjs/index.ts
+++ packages/interactjs/index.ts
@@ -1,12 +1,2 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import interact from '@interactjs/interactjs/index'
-
-export default interact
-
-if (typeof module === 'object' && !!module) {
-  try {
-    module.exports = interact
-  } catch {}
-}
-
-;(interact as any).default = interact
+export { default } from '@interactjs/interactjs/index'
