0b042eedf36eb209e0501729c3f725af04af39be
Tobias Speicher <rootcommander@gmail.com>
2022-03-18 11:12:03 +0100

    chore: replace deprecated String.prototype.substr()
    .substr() is deprecated so we replace it with .slice() which works similarily but isn't deprecated
    Signed-off-by: Tobias Speicher <rootcommander@gmail.com>

--- packages/@interactjs/core/isNonNativeEvent.ts
+++ packages/@interactjs/core/isNonNativeEvent.ts
@@ -6,7 +6,7 @@ export default function isNonNativeEvent (type: string, actions: Actions) {
   }
 
   for (const name in actions.map) {
-    if (type.indexOf(name) === 0 && type.substr(name.length) in actions.phases) {
+    if (type.indexOf(name) === 0 && type.slice(name.length) in actions.phases) {
       return true
     }
   }
